cd8eb8aa9098a25914620928c6feaef72a30d57d
es10130813 <espinkheart@gmail.com>
2024-01-05 21:03:45 +0900

    url 수정

--- lib/model/api.dart
+++ lib/model/api.dart
@@ -4,14 +4,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  static const baseUrl = "http://143.248.196.88/32/";
+  static const baseUrl = "http://143.248.196.88:3000/";
 
-  static adduser(Map udata) async{
+  static adduser(Map udata) async {
     print(udata);
     var url = Uri.parse("${baseUrl}register");
-    try {
 
-      final res = await http.post(Uri.parse("uri"), body: udata);
+    try {
+      final res = await http.post(url, body: udata);
       if(res.statusCode == 201){
         var data = jsonDecode(res.body.toString());
         print(data);
