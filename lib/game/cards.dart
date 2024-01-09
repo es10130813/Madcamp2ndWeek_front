a88adc6a5fc1fefa7d09aa578dd839c8d2d82a50
es10130813 <espinkheart@gmail.com>
2024-01-10 00:18:46 +0900

    카드 제출 구현

--- lib/game/cards.dart
+++ lib/game/cards.dart
@@ -83,8 +83,9 @@ class CardWidget extends StatelessWidget {
 class BuildDragTarget extends StatefulWidget {
   final List pile;
   List num;
+  List attacks;
 
-  BuildDragTarget({Key? key, required this.pile, required this.num}) : super(key: key);
+  BuildDragTarget({Key? key, required this.pile, required this.num, required this.attacks,}) : super(key: key);
 
   @override
   _BuildDragTargetState createState() => _BuildDragTargetState();
@@ -96,19 +97,32 @@ class _BuildDragTargetState extends State<BuildDragTarget> {
     return DragTarget<String>(
       onWillAccept: (data) {
         if (widget.num.length == 1) {
-          return (widget.pile.last[0] == data?[0] || widget.pile.last[1] == data?[1]);
+          if (widget.pile.last == "XR") return false;
+          else if (widget.pile.last == "XB") return (data=="XR"||data=="AS");
+          else if (widget.pile.last == "AS") return (data=="XR"||data=="XB");
+          else if (widget.pile.last != "AS"&&widget.pile.last[0]=="A") return (data=="XR"||data=="XB"||data=="AS");
+          else if (widget.pile.last[0]=="2") return (data=="XR"||data=="XB"||data?[0]=="A");
+          else {
+            return (widget.pile.last[0] == data?[0] || widget.pile.last[1] == data?[1]|| data == "XB" || data == "XR");
+          }
         }
         else if (widget.num.length == 0) {
-          return widget.pile.last[0] == data?[0];
+          return (widget.pile.last[0] == data?[0]);
         }
         else {
           return false;
         }
       },
+
       onAccept: (data) {
-        if (data?[0]=="K") widget.num.add("turn");
-        widget.pile.add(data);
         if (widget.num.isNotEmpty) widget.num.removeLast();
+        if (data[0]=="2") for(int i=0;i<2;i++) widget.attacks.add("attack");
+        if (data == "AD"||data=="AH"||data=="AC") for(int i=0;i<3;i++) widget.attacks.add("attack");
+        if (data == "AS"||data=="XB") for(int i=0;i<5;i++) widget.attacks.add("attack");
+        if (data == "XR") for(int i=0;i<8;i++) widget.attacks.add("attack");
+        widget.pile.add(data);
+        if (data[0]=="K") widget.num.add("turn");
+
         print(widget.pile);
         print(widget.num);
         print('Card $data dropped!');
